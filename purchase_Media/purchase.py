676dc6a729fec3abf1b2d4ea4e6b797e47cf0b6e
anil <anil@apagen.com>
2015-03-24 13:08:36 +0530

    purchase media module updation

--- purchase_Media/purchase.py
+++ purchase_Media/purchase.py
@@ -11,6 +11,7 @@ class purchase_requisition(osv.osv):
 
 	_columns = {
 		'creation_date': fields.datetime('Creation Date',required=True),
+		'user_id': fields.many2one('res.users', 'Responsible',required=True),
 		#'exclusive': fields.selection([('exclusive', 'Select only one RFQ (exclusive)'), ('multiple', 'Select multiple RFQ')], 'Requisition Type', required=True, help="Select only one RFQ (exclusive):  On the confirmation of a purchase order, it cancels the remaining purchase order.\nSelect multiple RFQ:  It allows to have multiple purchase orders.On confirmation of a purchase order it does not cancel the remaining orders"""),
 		#'origin': fields.char('source document'),
 		#'ordering_date' : fields.datetime('Requsition Date'),
