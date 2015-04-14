2b6970c132a397c27a4cc30f0a14b73fd6138297
Arun <arun.kumar@apagen.com>
2015-04-14 18:21:14 +0530

    abc

--- warehouse_media/__openerp__.py
+++ warehouse_media/__openerp__.py
@@ -1,10 +1,10 @@
 # Copyright 2014-2015 - Apagen Solutions Pvt. Ltd.
 {
-    'name': 'Warehouse_RAG',
+    'name': 'Warehouse_Media',
         'version': '1.0',
-        'category': 'Warehouse',
+        'category': 'Media Industry',
         'sequence': 14,
-        'summary': 'This application modifies warehouse management for media industry',
+        'summary': 'Media Industry',
 #        'description': """
  #       """,
         'author': 'Apagen Solution Pvt Ltd.',
@@ -14,8 +14,12 @@
     'init_xml': [
     ],
     'data': [
+        'wizard/stock_move_history_view.xml',
+        'report/report.xml',        
+    	'views/report_stockmove.xml',
     	'views/report_stockpicking.xml',
     	'views/report_stockinventory.xml',
+        'views/report_stockvaluation.xml',    	
         'warehouse_media_view.xml',
     	'stock_report.xml',
         
