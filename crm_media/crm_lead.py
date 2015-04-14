0c38c197783777798d478387fcc6ef6f3cafab94
Arun <arun.kumar@apagen.com>
2015-04-14 17:14:09 +0530

    abc

--- crm_media/crm_lead.py
+++ crm_media/crm_lead.py
@@ -33,20 +33,20 @@ class crm_lead(osv.osv):
         '''
         assert len(ids) == 1, 'This option should only be used for a single id at a time.'
         ir_model_data = self.pool.get('ir.model.data')
-        try:
-            template_id = ir_model_data.get_object_reference(cr, uid, 'sale', 'email_template_edi_sale')[1]
-        except ValueError:
-            template_id = False
+        #try:
+            #template_id = ir_model_data.get_object_reference(cr, uid, 'sale', 'email_template_edi_sale')[1]
+        #except ValueError:
+         #   template_id = False
         try:
             compose_form_id = ir_model_data.get_object_reference(cr, uid, 'mail', 'email_compose_message_wizard_form')[1]
         except ValueError:
             compose_form_id = False 
         ctx = dict()
         ctx.update({
-            'default_model': 'sale.order',
+            'default_model': 'crm.lead',
             'default_res_id': ids[0],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
+            #'default_use_template': bool(template_id),
+            #'default_template_id': template_id,
             'default_composition_mode': 'comment',
             'mark_so_as_sent': True
         })
