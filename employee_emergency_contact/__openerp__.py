5d46cfb056582ea263f9cf860eff06d37e7f5023
anil <anil@apagen.com>
2015-04-22 19:11:57 +0530

    [Add directore]

--- employee_emergency_contact/__openerp__.py
+++ employee_emergency_contact/__openerp__.py
@@ -9,7 +9,7 @@
     'website': 'http://www.apagen.com',
     'summary': '',
     'description': """This application extends the functionality of Employee Register""",
-    'depends': ['hr','employee_register_RAG','base'],
+    'depends': ['hr','employee_register_RAG','base', 'hr_security'],
     'data': [
     	'security/ir.model.access.csv',
         'emp_emergency_contact_view.xml',
